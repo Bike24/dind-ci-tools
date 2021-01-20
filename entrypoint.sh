462f6339e79cab2ee55318b5af6cc347857192a1
André Bauer <monotek23@gmail.com>
2021-01-20 13:38:20 +0100

    changed gpg fallback
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,26 @@
 
 set -e
 
+COMMAND="${1}"
+
 if [ -n "${GPG_PUB_KEYS}" ]; then
+
   for KEY in ${GPG_PUB_KEYS}; do
+
     echo "Fetch ${KEY} from hkp://p80.pool.sks-keyservers.net:80"
-    gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv ${KEY};
 
-    if [ $? != 0 ]; then
-      # Fallback if first fetch failed
+    if ! gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv "${KEY}"; then
       echo "Fallback: Fetch ${KEY} from hkp://ipv4.pool.sks-keyservers.net"
-      gpg --keyserver hkp://ipv4.pool.sks-keyservers.net --recv ${KEY};
-
-      if [ $? != 0 ]; then
-        # 2nd fallback
+    elif ! gpg --keyserver hkp://ipv4.pool.sks-keyservers.net --recv "${KEY}"; then
         echo "2nd fallback: Fetch ${KEY} from hkp://pgp.mit.edu:80"
-        gpg --keyserver hkp://pgp.mit.edu:80 --recv ${KEY};
-      fi
+    else 
+      gpg --keyserver hkp://pgp.mit.edu:80 --recv "${KEY}"
     fi
   done
 fi
 
-if [ -n "${1}" ]; then
-  ${1}
+if [ -n "${COMMAND}" ]; then
+  "${COMMAND}"
 else
   sh /data/commands.sh
 fi
