d42e196436a32edc6c4bf652acabb1c2c1719d97
André Bauer <monotek23@gmail.com>
2021-01-20 13:50:33 +0100

    fix shebang
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,8 @@ set -e
 COMMAND="${1}"
 
 if [ -n "${GPG_PUB_KEYS}" ]; then
-
   for KEY in ${GPG_PUB_KEYS}; do
-
     echo "Fetch ${KEY} from hkp://p80.pool.sks-keyservers.net:80"
-
     if ! gpg --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv "${KEY}"; then
       echo "Fallback: Fetch ${KEY} from hkp://ipv4.pool.sks-keyservers.net"
     elif ! gpg --keyserver hkp://ipv4.pool.sks-keyservers.net --recv "${KEY}"; then
