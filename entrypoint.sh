33887d9d925b4f0e565fe2811781a76cefea4e6f
André Bauer <monotek@users.noreply.github.com>
2021-01-21 17:59:35 +0100

    change gpg import fallback

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,15 @@ set -e
 COMMAND="${1}"
 KEYSERVER1="hkp://ipv4.pool.sks-keyservers.net"
 KEYSERVER2="hkp://keys.gnupg.net"
-KEYSERVER3="hkp://keyserver.ubuntu.com:80"
+KEYSERVER3="hkp://pgp.mit.edu:80"
+KEYSERVER4="hkp://keyserver.ubuntu.com:80"
 
 if [ -n "${GPG_PUB_KEYS}" ]; then
   for KEY in ${GPG_PUB_KEYS}; do
-    echo "Fetch ${KEY} from ${KEYSERVER1}"
-    if ! gpg --keyserver ${KEYSERVER1} --recv "${KEY}"; then
-      echo "Fallback: Fetch ${KEY} from ${KEYSERVER2}"
-      if ! gpg --keyserver ${KEYSERVER2} --recv "${KEY}"; then
-        echo "2nd fallback: Fetch ${KEY} from ${KEYSERVER3}"
-        gpg --keyserver ${KEYSERVER3} --recv "${KEY}"
-      fi
-    fi
+    gpg --batch --keyserver ${KEYSERVER1} --recv-keys "${KEY}" || \
+    gpg --batch --keyserver ${KEYSERVER2} --recv-keys "${KEY}" || \
+    gpg --batch --keyserver ${KEYSERVER3} --recv-keys "${KEY}" || \ 
+    gpg --batch --keyserver ${KEYSERVER4} --recv-keys "${KEY}" 
   done
 fi
 
